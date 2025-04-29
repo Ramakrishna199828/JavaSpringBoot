8c3a9d0b193f7fbd11486d071af12cb37a85c4b1
Ramakrishna199828 <rknayudu1@gmail.com>
2025-04-29 18:32:59 +0530

    Made changes in deploy.yml file

--- src/main/java/com/example/JavaSpringBootApplication/HomeController.java
+++ src/main/java/com/example/JavaSpringBootApplication/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String sayHello() {
-        return "Hello, World! Good EVening";
+        return "Hello, World! Good Evening";
     }
 }
