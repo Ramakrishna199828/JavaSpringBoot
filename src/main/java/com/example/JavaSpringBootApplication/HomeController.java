27837d7a172fa7e8311e1c84b22420cb3fc6f843
Ramakrishna199828 <rknayudu1@gmail.com>
2025-05-12 16:40:47 +0530

    Lastest changes to code on 16:40

--- src/main/java/com/example/JavaSpringBootApplication/HomeController.java
+++ src/main/java/com/example/JavaSpringBootApplication/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String sayHello() {
-        return "Hello,Good Evening Rama@!@";
+        return "Hello,Good Evening Anjali@!@";
     }
 }
