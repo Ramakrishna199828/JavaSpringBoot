b746c1fd188b4d057415bbc112b027a28bd4e5d9
Ramakrishna199828 <rknayudu1@gmail.com>
2025-05-12 15:34:46 +0530

    testing auto deployment

--- src/main/java/com/example/JavaSpringBootApplication/HomeController.java
+++ src/main/java/com/example/JavaSpringBootApplication/HomeController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String sayHello() {
-        return "Hello Ram,Good Afternoon@!@";
+        return "Hello,Good Afternoon Ramakrishna@!@";
     }
 }
